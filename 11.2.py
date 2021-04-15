31508883d4abe61cfae751c830bf66115adc5dc3
DOMOKUL <63143609+DOMOKUL@users.noreply.github.com>
2021-04-15 10:32:05 +0300

    Delete 11.2.py

--- 11.2.py
+++
@@ -1,7 +0,0 @@
-for _ in range(int(input())):
-    print(filter_(input()))
-    for i, x in enumerate(line):
-        if x == '#':
-            return ''
-        elif x != '%' :
-            return line[i:]
