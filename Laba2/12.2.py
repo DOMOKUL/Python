83e6bd293a9c661b157fdb4349446cb8d216cc33
pokal <strokachnik@gmail.com>
2022-03-01 18:31:22 +0300

    cleanup code

--- Laba2/12.2.py
+++ Laba2/12.2.py
@@ -6,12 +6,12 @@ lot, itogo = int(n[:4]), int(n[4:])
 for i in range(lot):
     a = input()
     price, kolvo, summ = int(a[0:7]), int(a[8:12]), int(a[13:18])
-    if price * kolvo != summ:
-        s.append(i + 1)
+    # if price * kolvo != summ:
+    #     s.append(i + 1)
     summ1 = kolvo * price
     s1.append(summ1)
 for i in s1:
     b += i
 print(itogo - b)
-for i in s:
-    print(i, end=' ')
+# for i in s:
+#     print(i, end=' ')
--- Laba2/13.1.py
+++ Laba2/13.1.py
@@ -1,3 +1,5 @@
+name=input()
+count= int(input())
 lol = [input() for _ in range(int(input()))]
 for _ in range(int(input())):
     tmp = [lol[int(input())-1] for _ in range(int(input()))]
